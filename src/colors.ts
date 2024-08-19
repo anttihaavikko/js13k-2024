12956afdc8f49e4abac6d58212dc4fef2d917c58
Antti Haavikko <antti.haavikko@gmail.com>
2024-08-19 12:14:07 +0300

    Add new skin color

--- src/colors.ts
+++ src/colors.ts
@@ -1,3 +1,4 @@
-export const skins = ['#e3e0bc', '#c9c595', '#edece1', '#dbac86', '#ebb7cc', '#734e3b', '#6e594f'];
+export const skins = ['#e3e0bc', '#c9c595', '#edece1', '#dbac86', '#ebb7cc', '#734e3b', '#6e594f', '#ede1e6'];
 export const woods = ['#a68b49', '#7d572c', '#633f15', '#8c8465'];
-export const fabrics = ['white', '#ed9828', '#9d55d4', '#24e387', '#6cd158', '#d420b6', '#333', '#fcf63d', '#f0ed92'];
+export const fabrics = ['#fff', '#ed9828', '#9d55d4', '#24e387', '#6cd158', '#d420b6', '#333', '#fcf63d', '#f0ed92'];
+// export const fabrics = ['#fff', '#e92', '#95d', '#2e8', '#6d5', '#d2b', '#333', '#ff3', '#fe9'];

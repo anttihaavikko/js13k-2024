3f21c09fb95a6d95ea6628d6b3e53b52d1421721
Antti Haavikko <antti.haavikko@gmail.com>
2024-08-20 08:37:34 +0300

    Remove default params

--- src/dude.ts
+++ src/dude.ts
@@ -31,11 +31,9 @@ export class Dude extends Entity {
         super(game, x, y, 0, 0);
         this.face = new Face(this.game, {
             blush: '#ed4ea3aa',
-            eyeSize: 10,
             width: 0.7,
-            mouthWidth: 0.5,
-            blushSize: 1,
-            mouthThickness: 12
+            mouthWidth: 0.75,
+            mouthThickness: 11
         });
         this.skin = randomCell(skins);
         this.animOffset = Math.random() * 9999;
